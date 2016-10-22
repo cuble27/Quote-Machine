4ba25931b3c029d848b6b315b364a02b97bbb0f0
Bogdan Cublesan <bogdan.cublesan@gmail.com>
2016-10-22 19:42:57 +0300

    Added some animations

--- myScript.js
+++ myScript.js
@@ -2,8 +2,18 @@
 
 var theQuote="";
 var theAuthor="";
+// $(document).ready(function() {
+//
+//    $('.newQuote').animate({  borderSpacing: -180 }, {
+//        step: function(now,fx) {
+//          $(this).css('transform','rotate('+now+'deg)');
+//        },
+//        duration:'slow'
+//    },'linear');
+// });
 
 function newQuote() {
+   $('.newQuote').addClass('spin');
    console.log("starts loading api");
    $.ajax({
       url: 'https://crossorigin.me/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
@@ -14,8 +24,20 @@ function newQuote() {
       success: function(quote){
          theQuote= quote[0].content;
          theAuthor= quote[0].title;
-         $('.quote').html('<p>'+theQuote+'</p>');
-         $('.author').html('<p>'+theAuthor+'</p>');
+         $('.quoteDiv').animate({height:"toggle"}, 600, function(){
+            $('.quote').html('<p>'+theQuote+'</p>');
+            $('.author').html('<p>'+theAuthor+'</p>');
+            $('.newQuote').removeClass('spin');
+            $('.quoteDiv').animate({height:"toggle"}, 600);
+         });
+
+         //       FIRST PROTOTYPE OF ANIMATION
+         // $('.quote, .author').fadeOut('400', function() {
+         //    $('.quote').html('<p>'+theQuote+'</p>');
+         //    $('.author').html('<p>'+theAuthor+'</p>');
+         //    $('.quote, .author').fadeIn('400');
+         // });
+
       },
       cache:false
    });
